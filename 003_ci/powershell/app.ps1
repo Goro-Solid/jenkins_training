72644166d3db7fbc37a2310a8585d7a792caa41e
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-29 13:50:45 +0100

    Biale znaki na potrzeby szkolenia

--- 003_ci/powershell/app.ps1
+++ 003_ci/powershell/app.ps1
@@ -1,6 +1,8 @@
 #
-function Select-Winner($Team1,$Team2){
-    If($Team1 -eq "Legia"){
+function Select-Winner($Team1,$Team2)
+{
+    If ($Team1 -eq "Legia")
+    {
         return $Team1
     }
     return $Team2
