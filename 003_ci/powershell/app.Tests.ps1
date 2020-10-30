7141229eac5c450bd1e039ced364c93d0ad06580
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2020-10-30 11:56:33 +0100

    Update app.Tests.ps1

--- 003_ci/powershell/app.Tests.ps1
+++ 003_ci/powershell/app.Tests.ps1
@@ -21,7 +21,7 @@ Describe "League tests" {
             Select-Winner -Team1 "Other" -Team2 "Widzew" | Should -be "Widzew"
         }
         It "new case" {
-            Select-Winner -Team1 "Other" -Team2 "Widzew" | Should -be "Widzew2"
+            Select-Winner -Team1 "Other" -Team2 "Widzew" | Should -be "Widzew"
         }
     }
 }
